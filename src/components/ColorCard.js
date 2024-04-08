ef2955d307d0640c565a8b0579294765fd81f046
Natascha Tenhagen <nattenhagen@googlemail.com>
2024-04-08 14:33:36 +0200

    fetch color name

--- src/components/ColorCard.js
+++ src/components/ColorCard.js
@@ -1,6 +1,27 @@
 import "./ColorCard.css";
 
+import React, { useState, useEffect } from "react";
+
 export function ColorCard({ color }) {
+  const [colorName, setColorName] = useState(null);
+
+  useEffect(() => {
+    fetchColorName(color.value);
+  });
+
+  async function fetchColorName() {
+    try {
+      const response = await fetch(
+        `https://www.thecolorapi.com/id?hex=${color.value.substring(1)}`
+      );
+      const data = await response.json();
+      setColorName(data.name.value);
+    } catch (error) {
+      console.error("Error fetching color name:", error);
+    }
+    fetchColorName();
+  }
+
   return (
     <div>
       <article className="color-card">
@@ -11,6 +32,7 @@ export function ColorCard({ color }) {
         <div className="color-card__info">
           <span className="color-card__role">{color.role}</span>
           <span className="color-card__hex">{color.value}</span>
+          <span className="color-card__name">{colorName}</span>
         </div>
       </article>
     </div>
